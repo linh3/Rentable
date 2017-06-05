3275e739d3d2d0e8489bfe08b0b204462ff27ff7
Leo <linh3@seattleu.edu>
2017-06-04 22:18:37 -0700

    e

--- googleOauth2.js
+++ googleOauth2.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class googleOauth2 {
 }
-googleOauth2.id = 'Google APP ID';
-googleOauth2.secret = 'Google APP SECRET';
+googleOauth2.id = '281682085717-h231lgr8vo7grcclmjni0qmveo4752mg.apps.googleusercontent.com';
+googleOauth2.secret = '7jeuUS6bvsUSMSJLitCN2BMg';
 exports.default = googleOauth2;
--- googleOauth2.ts
+++ googleOauth2.ts
@@ -1,5 +1,5 @@
 class googleOauth2 {
-  static id: string = 'Google APP ID';
-  static secret:string = 'Google APP SECRET';
+  static id: string = '281682085717-h231lgr8vo7grcclmjni0qmveo4752mg.apps.googleusercontent.com';
+  static secret:string = '7jeuUS6bvsUSMSJLitCN2BMg';
 }
 export default googleOauth2;
