41380252864d7975dbee342feb9328129c5d6536
Leo <linh3@seattleu.edu>
2017-06-05 15:08:40 -0700

    e

--- GooglePassport.ts
+++ GooglePassport.ts
@@ -19,7 +19,7 @@ export default class GooglePassport {
         passport.use(new GoogleStrategy({
                 clientID: this.clientId,
                 clientSecret: this.secretId,
-                callbackURL: "http://rentableappsu.azurewebsites.net/PostItem",
+                callbackURL: "http://rentableappsu.azurewebsites.net/auth/google/callback",
                 profileFields: ['id', 'displayName', 'emails']
             },
             (accessToken, refreshToken, profile, done) => {
