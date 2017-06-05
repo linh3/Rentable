706d67bb502ea3037287cae544c47fa60609565b
Leo <linh3@seattleu.edu>
2017-06-04 22:49:12 -0700

    e

--- App.ts
+++ App.ts
@@ -83,7 +83,7 @@ class App {
 
     router.get('/auth/google/callback', 
         passport.authenticate('google', 
-            { successRedirect: '/CreateAccount', failureRedirect: '/'
+            { successRedirect: '/CreateAccount/Creates', failureRedirect: '/'
             }
         )
     );
