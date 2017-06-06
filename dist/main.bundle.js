be48c999adcca232ca26e06c2a8c5b1c8a75de1e
Leo <linh3@seattleu.edu>
2017-06-06 00:31:21 -0700

    e

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -947,7 +947,7 @@ module.exports = module.exports.toString();
 /***/ "./src/app/postlist-component/postlsitresult-component/postlsitresult-component.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div id = \"search\" class=\"search\">\n  <h1 class=\"TitleFont\">{{username}}Your Posts...</h1>  \n  <!--<div *ngIf=\"resultList?.length == 0\">\n    <br><br><br><h1 class=\"TitleFont\">Sorry, You don't have any post</h1><br><br><br>\n  </div>\n  <a [routerLink] = \"['']\" class= \"searchContent\" *ngFor = \"let each of resultList\">\n    <img src=\"{{each.image}}\" class=\"searchImgs\">\n    <div class=\"TitleSubFont\">{{each.title}}</div>\n    <div class=\"ContentFont\">Price: ${{each.price}} per day<br>Location: {{each.location}}<br><br>Description: {{each.description}}</div>\n  </a>-->\n</div>\n"
+module.exports = "<div id = \"search\" class=\"search\">\n  <h1 class=\"TitleFont\">{{email}}Your Posts...</h1>\n  <h1 class=\"TitleFont\">{{userID}}</h1>  \n  <h1 class=\"TitleFont\">{{Fname}}</h1>  \n  <h1 class=\"TitleFont\">{{Lname}}</h1>  \n  <!--<div *ngIf=\"resultList?.length == 0\">\n    <br><br><br><h1 class=\"TitleFont\">Sorry, You don't have any post</h1><br><br><br>\n  </div>\n  <a [routerLink] = \"['']\" class= \"searchContent\" *ngFor = \"let each of resultList\">\n    <img src=\"{{each.image}}\" class=\"searchImgs\">\n    <div class=\"TitleSubFont\">{{each.title}}</div>\n    <div class=\"ContentFont\">Price: ${{each.price}} per day<br>Location: {{each.location}}<br><br>Description: {{each.description}}</div>\n  </a>-->\n</div>\n"
 
 /***/ }),
 
@@ -976,7 +976,12 @@ var PostlsitresultComponentComponent = (function () {
         var _this = this;
         this._router = _router;
         rentableService$.getUserInfo()
-            .subscribe(function (result) { _this.username = result.emails[0].value; });
+            .subscribe(function (result) {
+            _this.email = result.emails[0].value;
+            _this.userID = result.id;
+            _this.Lname = result.name.familyName;
+            _this.Fname = result.name.givenName;
+        });
     }
     PostlsitresultComponentComponent.prototype.ngOnInit = function () {
     };
