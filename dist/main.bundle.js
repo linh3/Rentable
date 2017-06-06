a036cc2ee52eed6c7c26090b3b64a2491ffecae3
Leo <linh3@seattleu.edu>
2017-06-06 11:15:11 -0700

    e

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -992,7 +992,7 @@ var PostlsitresultComponentComponent = (function () {
             _this.Fname = result.name.givenName;
             _this.JJ.fName = _this.Fname;
             //console.log("fname: " +this.JJ.fName);
-            rentableService$.checkUser(_this.userID)
+            rentableService$.checkUser("this.userID")
                 .subscribe(function (result) {
                 _this.userInfo = result;
                 console.log(_this.userInfo);
