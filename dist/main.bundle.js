b337f39000c61559f3bfefa1c7ea46d7ae49f903
Leo <linh3@seattleu.edu>
2017-06-06 02:09:18 -0700

    e

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -975,22 +975,16 @@ var PostlsitresultComponentComponent = (function () {
     function PostlsitresultComponentComponent(rentableService$, _router) {
         var _this = this;
         this._router = _router;
-        this.JJ = { "userID": "0", "email": "", "fName": "", "lName": "" };
         rentableService$.getUserInfo()
             .subscribe(function (result) {
-            _this.email = result.emails[0].value;
-            _this.JJ["email"] = result.emails[0].value;
-            _this.userID = result.id;
-            _this.JJ["userID"] = result.id;
-            _this.JJ["lName"] = result.name.familyName;
-            _this.JJ["fName"] = result.name.givenName;
-            _this.Lname = result.name.familyName;
-            _this.Fname = result.name.givenName;
+            _this.userInfo.email = result.emails[0].value;
+            _this.userInfo.userID = result.id;
+            _this.userInfo.lName = result.name.familyName;
+            _this.userInfo.fName = result.name.givenName;
         });
-        console.log("dasfasdf" + this.Fname);
         //var JO = {userID : this.userID, email: this.email, fName: this.Fname, lName: this.Lname};
-        console.log(this.JJ);
-        rentableService$.postAccountToDb(this.JJ)
+        console.log(JSON.stringify(this.userInfo));
+        rentableService$.postAccountToDb(JSON.stringify(this.userInfo))
             .subscribe(function (result) { });
     }
     PostlsitresultComponentComponent.prototype.ngOnInit = function () {
